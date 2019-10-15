3927f448a7f996ff3584df0f7866467e0d857612
david <davidndy@gmail.com>
2019-10-14 22:28:08 -0500

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,14 @@
 #                           PUT    /users(.:format)                                                                         devise/registrations#update
 #                           DELETE /users(.:format)                                                                         devise/registrations#destroy
 #                           POST   /users(.:format)                                                                         devise/registrations#create
+#                    movies GET    /movies(.:format)                                                                        movies#index
+#                           POST   /movies(.:format)                                                                        movies#create
+#                 new_movie GET    /movies/new(.:format)                                                                    movies#new
+#                edit_movie GET    /movies/:id/edit(.:format)                                                               movies#edit
+#                     movie GET    /movies/:id(.:format)                                                                    movies#show
+#                           PATCH  /movies/:id(.:format)                                                                    movies#update
+#                           PUT    /movies/:id(.:format)                                                                    movies#update
+#                           DELETE /movies/:id(.:format)                                                                    movies#destroy
 #        rails_service_blob GET    /rails/active_storage/blobs/:signed_id/*filename(.:format)                               active_storage/blobs#show
 # rails_blob_representation GET    /rails/active_storage/representations/:signed_blob_id/:variation_key/*filename(.:format) active_storage/representations#show
 #        rails_disk_service GET    /rails/active_storage/disk/:encoded_key/*filename(.:format)                              active_storage/disk#show
@@ -26,5 +34,6 @@
 Rails.application.routes.draw do
   root "landing#index"
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :movies
 end
